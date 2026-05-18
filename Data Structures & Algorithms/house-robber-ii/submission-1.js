b4135d607742beed29c951ce4c8eb924f8abf13b
Aanchal Shah <71883236+aanchalsh@users.noreply.github.com>
2026-05-18 07:47:10 +0530

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.js
@@ -0,0 +1,32 @@
+// array, dp : //houses are in a CIRCLE !! 
+// first and last house are neighbors
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+        if (nums.length === 1) {
+            return nums[0];
+        }
+
+        const robRange = (start, end) => {
+            let prev = 0;
+            let last = 0;
+
+            for (let i = start; i <= end; i++) {
+                let current = Math.max(prev, last + nums[i]);
+
+                last = prev;
+                prev = current;
+            }
+
+            return prev;
+        };
+
+        return Math.max(
+            robRange(0, nums.length - 2),
+            robRange(1, nums.length - 1)
+        );
+    }
+}
